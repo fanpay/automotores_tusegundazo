1ada20963a6c3ba077bf0fe8f611366b75f85e2f
Fabian Payan <f.payan@uniandes.edu.co>
2023-04-30 02:54:22 -0500

    [T03]: Se adiciona servicio para consultar lista de vehículos.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -12,6 +13,7 @@ import { VehiculoModule } from './vehiculo/vehiculo.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     VehiculoModule
   ],
   providers: [],
