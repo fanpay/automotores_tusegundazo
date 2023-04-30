169d17a5a3849857202b9805b8843643d42af4e6
Fabian Payan <f.payan@uniandes.edu.co>
2023-04-30 04:08:50 -0500

    [T04]: Se adiciona cálculo de totales por marca.

--- src/app/vehiculo/vehiculo-list/vehiculo-list.component.ts
+++ src/app/vehiculo/vehiculo-list/vehiculo-list.component.ts
@@ -9,6 +9,7 @@ import { VehiculoService } from '../vehiculo.service';
 })
 export class VehiculoListComponent implements OnInit {
   vehiculos: Array<Vehiculo> = [];
+  imagenUrl: string = 'assets/img/banner.png';
 
   constructor(private vehiculoService: VehiculoService) { }
 
@@ -22,5 +23,26 @@ export class VehiculoListComponent implements OnInit {
       this.vehiculos = vehiculos;
     });
   }
+
+  cantidadVehiculosPorMarca(vehiculos: Vehiculo[]): { marca: string, cantidad: number }[] {
+    const conteos: {[marca: string]: number} = {};
+    for (const vehiculo of vehiculos) {
+      if (conteos[vehiculo.marca]) {
+        conteos[vehiculo.marca]++;
+      } else {
+        conteos[vehiculo.marca] = 1;
+      }
+    }
+
+    //creación de objeto tipo key:value para iterar fácilmente en el HTML
+    const resultado = [];
+    for (const marca in conteos) {
+      if (conteos.hasOwnProperty(marca)) {
+        resultado.push({ marca: marca, cantidad: conteos[marca] });
+      }
+    }
+    return resultado;
+  }
+
 
 }
